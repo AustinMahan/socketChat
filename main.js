8a7c71fccc66e89cc3dc9214bbfec84263d1b1f7
AustinMahan <amahan0424@gmail.com>
2016-12-06 15:26:32 -0700

    polish

--- main.js
+++ main.js
@@ -4,21 +4,12 @@ var http = require('http');
 var cors = require('cors');
 
 var app = Express();
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 app.use(cors())
 
 var server = http.createServer(app);
 var io = Socket(server);
 
-// app.use(Express.static('public'));
-
 var messeges = []
-
 io.on('connection', function (socket) {
   console.log('user connected');
 
