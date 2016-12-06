0a68dbc7bc68ba980befd3afe964de6b408c37ff
AustinMahan <amahan0424@gmail.com>
2016-12-06 10:06:00 -0700

    things

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var cors = require('cors')
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*:*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST");
