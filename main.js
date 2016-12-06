bac906865cc88024a814dba4b6aa12fd38f09229
AustinMahan <amahan0424@gmail.com>
2016-12-06 11:46:00 -0700

    corss yet agian

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var Express = require('express');
 var Socket = require('socket.io');
 var http = require('http');
+var cors = require('cors');
 
 var app = Express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+app.use(cors())
 
 var server = http.createServer(app);
 var io = Socket(server);
