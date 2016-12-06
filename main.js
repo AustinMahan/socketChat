59d0960e458631d557c2188cf14868e4e231ff54
AustinMahan <amahan0424@gmail.com>
2016-12-06 10:02:05 -0700

    another cors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var cors = require('cors')
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -7,8 +8,12 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST");
   next();
 });
-var server = http.createServer(app);
-io = socketio.listen(server, {log:false, origins:'*:*'});
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server, {log:false, origins:'*:*'});
+
+
+var messeges = []
+
 io.on('connection', function (socket) {
   console.log('user connected');
 
