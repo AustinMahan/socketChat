90baf859d520a8e00b5523d3097b56cf781d53f8
AustinMahan <amahan0424@gmail.com>
2016-12-06 09:41:48 -0700

    cors

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var app = require('express')();
+var cors = require('cors')
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(8080);
+app.use(cors)
 
 var messeges = []
 
