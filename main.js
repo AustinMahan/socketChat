78467bb5df2484fbd904f2eba18bbffbfd7e150e
AustinMahan <amahan0424@gmail.com>
2016-12-06 09:43:07 -0700

    attempting cors

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var cors = require('cors')
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+
 
 server.listen(8080);
-app.use(cors)
-
+server.use(cors)
+var io = require('socket.io')(server);
 var messeges = []
 
 io.on('connection', function (socket) {
